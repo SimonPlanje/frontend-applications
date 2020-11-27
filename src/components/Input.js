29bdaad7f22e292e09d41aff2ca9fcec3748087a
Simon Planje <simon.planje2000@gmail.com>
2020-11-27 13:08:19 +0100

    added researsch question to the top of the page

--- src/components/Input.js
+++ src/components/Input.js
@@ -18,6 +18,8 @@ const WalkCounter = ({numberState, numberUpdate, setMeer}) => {
 
  return(
   <div className='Walkcounter'>
+  <h1 >Waar kunnen we in tijden van corona onze energie nog kwijt?</h1>
+  <h1 className='hMargin' >Op welke parkeerplaatsen in Nederland kan ik het best parkeren om een mooie wandeling te maken?</h1>
   <h2> Hoeveel wandelingen maak je gemiddeld per maand? </h2>
   <form onSubmit={submitHandler} >
   <input className='numberForm' max="9" min="0" type="number" value={numberState} onChange={userInputHandler} autofocus />
